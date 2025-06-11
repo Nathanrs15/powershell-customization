82ba409bae47be967fe210b9d5db00d27f5d34d6
Nathanrs15 <49479748+Nathanrs15@users.noreply.github.com>
2025-06-11 13:33:14 +0200

    Update user_profile.ps1

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -3,6 +3,9 @@
 # i.e of theme from oh my posh webiste
 # oh-my-posh init pwsh --config "$(scoop prefix oh-my-posh)\themes\paradox.omp.json" | Invoke-Expression
 
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
+
 $omp_config = Join-Path $PSScriptRoot ".\takuya.omp.json"
 oh-my-posh init pwsh --config $omp_config  | Invoke-Expression
 
@@ -19,20 +22,9 @@ Import-Module PSFzf
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
 
 # Alias
-Set-Alias vim nvim
 Set-Alias g git
 Set-Alias ll ls
 Set-Alias grep findstr
-Set-Alias tig 'E:\Program Files\Git\usr\bin\tig.exe'
-Set-Alias less 'E:\Program Files\Git\usr\bin\less.exe'
-
-# Utilities
-function which ($command) {
-  Get-Command -Name $command -ErrorAction SilentlyContinue |
-    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
-}
-
-function gcmt([string]$message) {
-	git commit -m $message
-}
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
 
